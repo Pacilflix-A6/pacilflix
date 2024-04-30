4a5bf9261518cc43b2a62907e6799fa622aaaf83
lontonggg <zadareyhan@gmail.com>
2024-04-30 15:46:24 +0700

    Make changes in project settings

--- pacilflix/settings.py
+++ pacilflix/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts'
+    'accounts',
+    'daftar_kontributor'
 ]
 
 MIDDLEWARE = [
--- pacilflix/urls.py
+++ pacilflix/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
+    path('', include('daftar_kontributor.urls'))
 ]
