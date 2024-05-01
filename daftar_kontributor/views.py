d380887ab46457f08b6acc0006a22b7a895730ce
lontonggg <zadareyhan@gmail.com>
2024-05-01 14:35:37 +0700

    Added html skeleton for daftar kontributor page

--- daftar_kontributor/views.py
+++ daftar_kontributor/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def daftar_kontributor_page(request):
-    return render(request, 'index.html')
+    return render(request, 'daftar_kontributor.html')
