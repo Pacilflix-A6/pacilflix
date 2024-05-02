361db30e3c40bfc41881d1e1b39445cbff8f2f87
m-hilmy-erryanto <muhammad.hilmy22@ui.ac.id>
2024-05-02 11:03:51 +0700

    adedd navbar and base.html

--- pacilflix/settings.py
+++ pacilflix/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'pacilflix.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
