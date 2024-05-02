5ca19b93b4beb8774da06feb9ba35d7aab289360
FBimo <f.bimo31@gmail.com>
2024-05-02 10:58:46 +0700

    Add tayangan configuration to pacilflix

--- pacilflix/urls.py
+++ pacilflix/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
+    path('tayangan/', include('tayangan.urls')),
 ]
