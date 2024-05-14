8019f109b33b87d4ee4ab4c4caf78fe8cc9ac6b8
lontonggg <zadareyhan@gmail.com>
2024-05-14 18:42:49 +0700

    Fix function to handle contributor with multiple types

--- daftar_kontributor/views.py
+++ daftar_kontributor/views.py
@@ -8,18 +8,27 @@ def daftar_kontributor_page(request):
 def get_all_contributors():
     cursor = connection.cursor()
     cursor.execute(
-        """
-        SELECT c.id, c.nama, c.jenis_kelamin, c.kewarganegaraan,
-            CASE
-                WHEN ps.id IS NOT NULL THEN 'Penulis Skenario'
-                WHEN pm.id IS NOT NULL THEN 'Pemain'
-                WHEN st.id IS NOT NULL THEN 'Sutradara'
-                ELSE 'Unknown'
-            END AS tipe
-        FROM CONTRIBUTORS c
-        LEFT JOIN PENULIS_SKENARIO ps ON c.id = ps.id
-        LEFT JOIN PEMAIN pm ON c.id = pm.id
-        LEFT JOIN SUTRADARA st ON c.id = st.id
+         """
+        SELECT contributors.id, contributors.nama, contributors.jenis_kelamin, contributors.kewarganegaraan,
+            STRING_AGG(type_name, ', ') AS tipe
+        FROM (
+            SELECT c.id, c.nama, c.jenis_kelamin, c.kewarganegaraan,
+                CASE WHEN ps.id IS NOT NULL THEN 'Penulis Skenario' ELSE NULL END AS type_name
+                FROM CONTRIBUTORS c
+                LEFT JOIN PENULIS_SKENARIO ps ON c.id = ps.id
+            UNION ALL
+            SELECT c.id, c.nama, c.jenis_kelamin, c.kewarganegaraan,
+                CASE WHEN pm.id IS NOT NULL THEN 'Pemain' ELSE NULL END AS type_name
+                FROM CONTRIBUTORS c
+                LEFT JOIN PEMAIN pm ON c.id = pm.id
+            UNION ALL
+            SELECT c.id, c.nama, c.jenis_kelamin, c.kewarganegaraan,
+                CASE WHEN st.id IS NOT NULL THEN 'Sutradara' ELSE NULL END AS type_name
+                FROM CONTRIBUTORS c
+                LEFT JOIN SUTRADARA st ON c.id = st.id
+        ) AS types
+        LEFT JOIN CONTRIBUTORS ON types.id = contributors.id
+        GROUP BY contributors.id, contributors.nama, contributors.jenis_kelamin, contributors.kewarganegaraan
         """
     )
     
