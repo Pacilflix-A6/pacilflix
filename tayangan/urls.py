1d8b90967c66618a63eebf2b792cd5f2efe52940
FBimo <f.bimo31@gmail.com>
2024-05-17 22:53:20 +0700

    [Add]Read data from each film including slider feature

--- tayangan/urls.py
+++ tayangan/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
-from tayangan.views import tayangan, trailer, film, series, episode
+from tayangan.views import tayangan, trailer, film, series, episode, film_select, tonton, ulas
 
 app_name = 'tayangan'
 
 urlpatterns = [
     path('trailer/', trailer, name='trailer'),
     path('', tayangan, name='tayangan'),
+    path('film/<uuid:film_id>/', film_select, name='film_select'),
     path('film/', film, name='film'),
     path('series/', series, name='series'),
     path('episode/', episode, name='episode'),
+    path('film/<uuid:film_id>/tonton/', tonton, name='tonton'),
+    path('film/<uuid:film_id>/ulas/', ulas, name='ulas')
+    # path('film/<uuid:film_id>/', film_detail, name='film_detail'),
 ]
--- tayangan/views.py
+++ tayangan/views.py
@@ -1,5 +1,11 @@
+from datetime import datetime, timedelta
+import json
+
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.db import connection
+
+from accounts.sharedpref import LoggedInUser
 
 # Create your views here.
 
@@ -16,7 +22,7 @@ def tayangan(request):
 
     # Query untuk mengambil data film
     cursorb.execute(f"""
-        SELECT t.judul, t.sinopsis, t.url_video_trailer, f.release_date_film
+        SELECT t.judul, t.sinopsis, t.url_video_trailer, f.release_date_film, t.id
         FROM FILM AS f
         JOIN TAYANGAN AS t ON f.id_tayangan = t.id;
         """)
@@ -24,7 +30,7 @@ def tayangan(request):
     
     # Query untuk mengambil data series
     cursorb.execute("""
-        SELECT t.judul, t.sinopsis, t.url_video_trailer, t.release_date_trailer
+        SELECT t.judul, t.sinopsis, t.url_video_trailer, t.release_date_trailer, t.id
         FROM SERIES AS s
         JOIN TAYANGAN AS t ON s.id_tayangan = t.id;
     """)
@@ -37,7 +43,7 @@ def trailer(request):
 
     # Query untuk mengambil data film
     cursorb.execute(f"""
-        SELECT t.judul, t.sinopsis_trailer, t.url_video_trailer, t.release_date_trailer
+        SELECT t.judul, t.sinopsis_trailer, t.url_video_trailer, t.release_date_trailer, t.id
         FROM FILM AS f
         JOIN TAYANGAN AS t ON f.id_tayangan = t.id;
         """)
@@ -45,7 +51,7 @@ def trailer(request):
     
     # Query untuk mengambil data series
     cursorb.execute("""
-        SELECT t.judul, t.sinopsis_trailer, t.url_video_trailer, t.release_date_trailer
+        SELECT t.judul, t.sinopsis_trailer, t.url_video_trailer, t.release_date_trailer, t.id
         FROM SERIES AS s
         JOIN TAYANGAN AS t ON s.id_tayangan = t.id;
     """)
@@ -53,15 +59,109 @@ def trailer(request):
 
     return render(request, "trailer.html", {"films": films, "series": series})
 
+def film_select(request, film_id):
+    cursorb = connection.cursor()
+    cursorb.execute(f"""
+        SELECT 
+            f.id_tayangan,
+            t.judul AS judul_tayangan, 
+            t.sinopsis AS sinopsis_tayangan,
+            t.asal_negara,
+            f.url_video_film, 
+            f.release_date_film,
+            f.durasi_film,
+            ROUND(AVG(u.rating), 1) AS rating_rata_rata,
+            ARRAY_AGG(DISTINCT gt.genre) AS genre,
+            ARRAY_AGG(DISTINCT cpm.nama) AS pemain,
+            ARRAY_AGG(DISTINCT psc.nama) AS penulis_skenario,
+            cs.nama AS sutradara,
+            tv.total_view
+        FROM TAYANGAN AS t
+        JOIN FILM AS f ON t.id = f.id_tayangan
+        LEFT JOIN ULASAN AS u ON t.id = u.id_tayangan
+        LEFT JOIN GENRE_TAYANGAN as gt ON t.id = gt.id_tayangan
+        LEFT JOIN MENULIS_SKENARIO_TAYANGAN AS mst ON t.id = mst.id_tayangan
+        LEFT JOIN PENULIS_SKENARIO AS ps ON mst.id_penulis_skenario = ps.id
+        LEFT JOIN CONTRIBUTORS AS psc ON ps.id = psc.id
+        LEFT JOIN SUTRADARA AS p ON t.id_sutradara = p.id
+        LEFT JOIN CONTRIBUTORS AS cs ON p.id = cs.id
+        LEFT JOIN MEMAINKAN_TAYANGAN AS mt ON t.id = mt.id_tayangan
+        LEFT JOIN PEMAIN AS pm ON mt.id_pemain = pm.id
+        LEFT JOIN CONTRIBUTORS AS cpm ON pm.id = cpm.id
+        LEFT JOIN FILM_VIEWERS AS tv ON t.id = tv.id_tayangan
+
+        WHERE t.id = '{film_id}'
+        GROUP BY f.id_tayangan, t.judul, t.sinopsis, t.asal_negara, f.url_video_film, f.release_date_film, f.durasi_film, cs.nama, psc.nama, tv.total_view;
+        """)
+    film = dictfetchall(cursorb)[0]
+
+    # print(film)
+    
+    return render(request, 'film.html', {'film': film})
+
+def tonton(request, film_id):
+    cursor = connection.cursor()
+
+    if request.method == "POST":
+        # id_tayangan = request.body[0]
+        # print(request.body)
+        # print(id_tayangan)
+        # Mendapatkan data JSON dari request body
+        json_data = json.loads(request.body)
+        
+        # Mendapatkan nilai id_tayangan dan durasi_tonton dari data JSON
+        id_tayangan = json_data.get("id_tayangan")
+        durasi_tonton = json_data.get("durasi_tonton")
+        username = LoggedInUser.username
+        start_date_time = datetime.now()
+        end_date_time = start_date_time + timedelta(minutes=int(durasi_tonton))
+        
+        cursor.execute(f"""
+            INSERT INTO RIWAYAT_NONTON (id_tayangan, username, start_date_time, end_date_time)
+            VALUES (%s, %s, %s, %s);
+            """, (id_tayangan, username, start_date_time, end_date_time))
+        
+
+        return JsonResponse({"status": "success"})
+    
+    return JsonResponse({"status": "failed"})
+
+def ulas(request, film_id):
+    cursor = connection.cursor()
+
+    if request.method == "POST":
+        # id_tayangan = request.body[0]
+        # print(request.body)
+        # print(id_tayangan)
+        # Mendapatkan data JSON dari request body
+        json_data = json.loads(request.body)
+        
+        # Mendapatkan nilai id_tayangan dan durasi_tonton dari data JSON
+        id_tayangan = film_id
+        username = LoggedInUser.username
+        timestamp = datetime.now()
+        rating = json_data.get("rating")
+        deskripsi = json_data.get("deskripsi")
+
+        cursor.execute(f"""
+            NSERT INTO ULASAN (id_tayangan, username, timestamp, rating, deskripsi)
+            VALUES (%s, %s, %s, %s, %s);
+            """, (id_tayangan, username, timestamp, rating, deskripsi))
+        
+
+        return JsonResponse({"status": "success"})
+    
+    return JsonResponse({"status": "failed"})
+
 def film(request):
 
     # ini buat dropdown daftar favorit di modal favorit
     cursorb = connection.cursor()
-    cursorb.execute(f"""
+    cursorb.execute("""
         SELECT judul, timestamp
         FROM DAFTAR_FAVORIT
-        WHERE username = 'john_doe';
-        """
+        WHERE username = %s;
+        """, [LoggedInUser.username]
     )
 
     context = {
@@ -74,11 +174,11 @@ def series(request):
 
     # ini buat dropdown daftar favorit di modal favorit
     cursorb = connection.cursor()
-    cursorb.execute(f"""
+    cursorb.execute("""
         SELECT judul, timestamp
         FROM DAFTAR_FAVORIT
-        WHERE username = 'john_doe';
-        """
+        WHERE username = %s;
+        """, [LoggedInUser.username]
     )
 
     context = {
